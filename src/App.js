a94738945a3e0d17e5178bdca445799600108393
ajithkumars-mps <ajithkumar.@mpslimited.com>
2024-05-16 15:48:46 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes, Redirect } from 'react-router-dom';
 import './App.css';
+import Login from './Pages/Login/Login';
+import Home from './Pages/Home/Home';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Registration from './Pages/Registration/Registration';
+import { useState } from 'react';
 
 function App() {
+  const [state,setState] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {
+    //   <Redirect
+    //   to={{
+    //     pathname: "/",
+    //   }}
+    // />
+    }
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>} />
+          <Route path='/home' element={<Home/>} />
+          <Route path='/dashboard' element={<Dashboard/>} />
+          <Route path='/registration' element={<Registration/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+
+
+
